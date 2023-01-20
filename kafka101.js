6de009f3ca2deff3381b3b18302fd6b33bf4a5f4
Mark Murphy <mark.murphy@techemail.com>
2023-01-20 11:13:44 +0000

    Added topology diagram to explainer output

--- kafka101.js
+++ kafka101.js
@@ -7,3 +7,32 @@ console.log(newLine);
 
 console.log(`This project contains simple examples of creating ${chalk.yellow('Publishers')} and ${chalk.yellow('Consumers')}`);
 console.log(`for Apache Kafka hosted in ${chalk.cyan('Confluent Cloud')}`);
+
+console.log(newLine);
+
+console.log('               ┌────────────┐');
+console.log('               │            │');
+console.log('      ┌────────►   Kafka    ├───────┐');
+console.log('      │        │            │       │');
+console.log('      │        └────────────┘       │');
+console.log('      │                             │');
+console.log('      │                             │');
+console.log('      │                             │');
+console.log('      │                             │');
+console.log('      │                             │');
+console.log('      │                             │');
+console.log('      │                             │');
+console.log('┌─────┴─────┐                 ┌─────▼────┐');
+console.log('│           │                 │          │');
+console.log('│ Publisher │                 │ Consumer │');
+console.log('│           │                 │          │');
+console.log('└─────▲─────┘                 └─────┬────┘');
+console.log('      │                             │');
+console.log('      │                             │');
+console.log('┌─────┴─────┐                 ┌─────▼────┐');
+console.log('│  Source   │                 │  Target  │');
+console.log('│  Folder   │                 │  Folder  │');
+console.log('│           │                 │          │');
+console.log('└───────────┘                 └──────────┘');
+
+console.log(newLine);
